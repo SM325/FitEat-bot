73168f53bfba6eae5b16b58d54d5f3cce6a6b2cf
rachel123nkr <44622520+rachel123nkr@users.noreply.github.com>
2020-11-17 09:47:55 +0200

    basic

---
+++ webhook_setup.py
@@ -0,0 +1,10 @@
+from config import TOKEN, NGROK_URL
+import requests
+
+TELEGRAM_BASE = 'https://api.telegram.org'
+
+TELEGRAM_INIT_WEBHOOK_URL = f'{TELEGRAM_BASE}/bot{TOKEN}/setWebhook?url={NGROK_URL}'
+
+# TELEGRAM_SEND_WEBHOOK_URL = '{}/bot{}/sendMessage?'.format(TELEGRAM_BASE, TOKEN)
+
+requests.get(TELEGRAM_INIT_WEBHOOK_URL)
