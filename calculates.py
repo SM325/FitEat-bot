1996bef792bb5d8e41747c3be562ce5b02b4b7f4
rachel123nkr <44622520+rachel123nkr@users.noreply.github.com>
2020-11-17 17:37:01 +0200

    marge calculate

--- calculates.py
+++ calculates.py
@@ -5,8 +5,8 @@ def age_by_birthday(bday):
     return datetime.datetime.now().year - bday.year
 
 
-def calculate_calories(height, weight, age, gender):
-    val = 0
+def get_calories(birth_date, weight, height, gender):
+    age = age_by_birthday(birth_date)
     if gender == "male":
         val = 66.5 + (13.75 * weight) + (5.003 * height) - (6.775 * age)
     else:
@@ -14,18 +14,21 @@ def calculate_calories(height, weight, age, gender):
     return int(1.2 * val)
 
 
-
-x = calculate_calories(1.74, 72.4, 45, "female")
-
-
-def get_calories(birth_date, weight, height, gender):
-    return 100
-
 def get_carbs(birth_date, weight, height, gender):
-    return 100
+    return int(get_calories(birth_date, weight, height, gender) * 0.5)
+
 
 def get_protein(birth_date, weight, height, gender):
-    return 100
+    return int(get_calories(birth_date, weight, height, gender) * 0.3)
+
 
 def get_fats(birth_date, weight, height, gender):
-    return 100
+    return int(get_calories(birth_date, weight, height, gender) * 0.2)
+
+ca = get_calories(datetime.datetime(1998, 3, 1), 70, 1.70, "female")
+car = get_carbs(datetime.datetime(1998, 3, 1), 70, 1.70, "female")
+p = get_protein(datetime.datetime(1998, 3, 1), 70, 1.70, "female")
+f = get_fats(datetime.datetime(1998, 3, 1), 70, 1.70, "female")
+
+
+print(ca, car, p, f)
