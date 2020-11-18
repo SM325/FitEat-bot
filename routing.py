4e3edb39e02b40603d71de5d6a3c3939c23646f3
תמר גולדשמיד <tgoldshm@g.jct.ac.il>
2020-11-18 17:42:59 +0200

    get_bmi

--- routing.py
+++ routing.py
@@ -238,8 +238,8 @@ def getBMI_handler(message):
     if message.is_exist_init_user():
         bmi = calculations.calculate_bmi(user['weight'], user['height'])
         normal_weight = calculations.calculate_normal_weight(user['height'])
-        bmi_print = "Your BMI is: " + str(bmi)
-        normal_weight_print = "\nNormal weight for your height is:\n" + str(normal_weight[0]) + " to " + str(
+        bmi_print = "<b><ins>Your BMI is:</ins></b> " + str(bmi)
+        normal_weight_print = "\n<b>Normal weight for your height is:</b>\n" + str(normal_weight[0]) + " to " + str(
             normal_weight[1])
         bmi_category = calculations.get_bmi_category(bmi)
         return bmi_print + "\n" + bmi_category + "\n" + normal_weight_print
